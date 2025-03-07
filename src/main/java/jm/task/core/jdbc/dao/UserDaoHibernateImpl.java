e1ec9936e2ac53dbdd917ac495723bd64f3a5405
TechEvgeny <v.c_kokoc@mail.ru>
2025-03-07 17:40:48 +0300

    PP1.1.4first

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,8 +1,12 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.List;
+import org.hibernate.Query;
 
 public class UserDaoHibernateImpl implements UserDao {
     public UserDaoHibernateImpl() {
@@ -12,8 +16,19 @@ public class UserDaoHibernateImpl implements UserDao {
 
     @Override
     public void createUsersTable() {
-
+        Session session = Util.getSessionFactory().openSession();
+        Transaction transaction = session.beginTransaction();
+        String createUsersTable = "CREATE TABLE IF NOT EXISTS users"
+//                +"(ID INTEGER PRIMARY KEY auto_increment, "
+                +"NAME VARCHAR(255) NOT NULL, "
+                +"LASTNAME VARCHAR(255) NOT NULL, "
+                +"AGE INT NOT NULL)";
+        Query query = session.createSQLQuery(createUsersTable).addEntity(User.class);
+        transaction.commit();
+        session.close();
     }
+
+
 
     @Override
     public void dropUsersTable() {
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,9 +1,18 @@
 package jm.task.core.jdbc.util;
 
 
+import org.hibernate.SessionFactory;
+import jm.task.core.jdbc.model.User;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.service.ServiceRegistry;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+
 
 
 public class Util {
@@ -12,16 +21,50 @@ public class Util {
     private static final String USER = "root117";
     private static final String PASSWORD = "root";
     // реализуйте настройку соеденения с БД
-    public static Connection getConnection() {
-        Connection connection = null;
-        try {
-            connection = DriverManager.getConnection(URL, USER, PASSWORD);
-            System.out.println("Connected to the database successfully");
+//    public static Connection getConnection() {
+//        Connection connection = null;
+//        try {
+//            connection = DriverManager.getConnection(URL, USER, PASSWORD);
+//            System.out.println("Connected to the database successfully");
+//
+//        } catch (SQLException e) {
+//            System.out.println("Failed to connect to database");
+//        }
+//        return connection;
+//
+//    }
 
-        } catch (SQLException e) {
-            System.out.println("Failed to connect to database");
+    private static SessionFactory sessionFactory;
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+
+                Configuration configuration = new Configuration();
+                Properties properties = new Properties();
+                properties.put(Environment.DRIVER, "com.mysql.jdbc.Driver");
+                properties.put(Environment.URL, URL);
+                properties.put(Environment.USER, USER);
+                properties.put(Environment.PASS, PASSWORD);
+                properties.put(Environment.DIALECT, "org.hibernate.dialect.MySQLDialect");
+                properties.put(Environment.HBM2DDL_AUTO, "create-drop");
+                properties.put(Environment.SHOW_SQL, "true");
+                properties.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+
+                configuration.setProperties(properties);
+                configuration.addAnnotatedClass(User.class);
+
+                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties()).build();
+
+                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+                System.out.println("Connected to the database successfully");
+
+            }
+            catch (Exception e) {
+                System.out.println("Failed to create session factory " + e.getMessage());
+            }
         }
-        return connection;
-
+        return sessionFactory;
     }
+
 }
